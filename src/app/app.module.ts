f4c755ee640cc90672a0ba5f7befc5b012f74186
Debasis Kar <debasiskar007@gmail.com>
2020-06-06 20:57:35 +0530

    credential helper commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialModule } from './thirdpartymodules/material-modules';
+import { from } from 'rxjs';
+import { HasmemoryleakComponent } from './jsmemoryleaks/hasmemoryleak/hasmemoryleak.component';
+import { Page1Component } from './jsmemoryleaks/hasmemoryleak/page1/page1.component';
+import { Page2Component } from './jsmemoryleaks/hasmemoryleak/page2/page2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HasmemoryleakComponent,
+    Page1Component,
+    Page2Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
