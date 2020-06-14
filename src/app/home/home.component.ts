259fa9d8274a588d3e8d27af705f82365c717ebb
Debasis Kar <debasiskar007@gmail.com>
2020-06-14 23:35:37 +0530

    meta service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,13 +11,13 @@ export class HomeComponent implements OnInit {
   constructor(private metaservice: MetaserviceService) { }
 
   ngOnInit() {
-    const data: object = {
-      title: 'Test title 1 ',
-      og_url: 'https://www.betoparedes.com/ ',
-      og_type: 'blog',
-      og_title: 'Angular Example 1',
-    };
-    this.metaservice.setmeta(data);
+    // const data: object = {
+    //   title: 'Test title 1 ',
+    //   og_url: 'https://www.betoparedes.com/ ',
+    //   og_type: 'blog',
+    //   og_title: 'Angular Example 1'
+    // };
+    // this.metaservice.setmeta(data);
   }
 
 }
